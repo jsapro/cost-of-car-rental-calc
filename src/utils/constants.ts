b0e93017745ba38bc49e5040a9925a52901f4c78
jsapro <77.3.77@mail.ru>
2023-09-19 14:28:43 +0300

    add constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,50 @@
+const cars = [
+    {
+      model: 'BMW X5',
+      class: 'B',
+      rentCostForDay: {
+        shortRent: 10000,
+        averageRent: 8000,
+        longRent: 5000,
+      },
+    },
+    {
+      model: 'Nissan Quashkai',
+      class: 'B',
+      rentCostForDay: {
+        shortRent: 9000,
+        averageRent: 7000,
+        longRent: 4500,
+      },
+    },
+    {
+      model: 'Ford Focus',
+      class: 'B',
+      rentCostForDay: {
+        shortRent: 8000,
+        averageRent: 6500,
+        longRent: 4000,
+      },
+    },
+    {
+      model: 'Ford Transit',
+      class: 'C',
+      rentCostForDay: {
+        shortRent: 12000,
+        averageRent: 10000,
+        longRent: 8000,
+      },
+    },
+    {
+      model: 'Mercedes-Benz Sprinter',
+      class: 'C',
+      rentCostForDay: {
+        shortRent: 13000,
+        averageRent: 11000,
+        longRent: 9000,
+      },
+    },
+];
+
+export {cars}
+  
